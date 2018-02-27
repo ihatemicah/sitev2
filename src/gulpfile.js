d178c5265bcbba0ce83bb5c7c36ad2a095bda7e6
Micah Carroll <micahcarroll@micahs-mbp.lan>
2018-02-26 21:12:00 -0800

    built dev enviornment, now working successfully

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,50 +1,59 @@
 
 // Include gulp
 var gulp = require('gulp');
- // Include plugins
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
-var rename = require('gulp-rename');
-var concatCss = require('gulp-concat-css');
+var useref = require('gulp-useref');
 var browserSync = require('browser-sync').create();
 
- // Concatenate JS Files & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src(['js/jquery-3.2.1.min.js','js/smooth-scroll.min.js','js/wow.min.js','js/particles.js','js/main.js','js/*.js'])
-      .pipe(concat('all.js'))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest('../docs/js'));
-});
+ // Include plugins
+// var sass = require('gulp-sass');
+// var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+// var cleanCSS = require('gulp-clean-css');
+// var rename = require('gulp-rename');
+// var concatCss = require('gulp-concat-css');
 
-// Compile SASS Files
-gulp.task('sass', function (){
-  return gulp.src('sass/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./styles'));
-});
-gulp.task('sass:watch', function () {
-  gulp.watch('../sass/**/*.scss',['sass']);
-});
+//  // Concatenate JS Files & Minify JS
+// gulp.task('scripts', function() {
+//     return gulp.src(['js/jquery-3.2.1.min.js','js/smooth-scroll.min.js','js/wow.min.js','js/particles.js','js/main.js','js/*.js'])
+//       .pipe(concat('all.js'))
+//       .pipe(rename({suffix: '.min'}))
+//       .pipe(uglify())
+//       .pipe(gulp.dest('../docs/js'));
+// });
 
-// Concatenate CSS Files
-gulp.task('css', function () {
-  return gulp.src(['styles/*.css','node_modules/normalize.css/normalize.css'])
-    .pipe(concatCss("all.css"))
-    .pipe(gulp.dest('../docs/styles'));
-});
+// // Compile SASS Files
+// gulp.task('sass', function (){
+//   return gulp.src('sass/**/*.scss')
+//   .pipe(sass().on('error', sass.logError))
+//   .pipe(gulp.dest('./styles'));
+// });
+// gulp.task('sass:watch', function () {
+//   gulp.watch('../sass/**/*.scss',['sass']);
+// });
 
-// Minify CSS (packs for file size.)
-gulp.task('minify-css', () => {
-  return gulp.src('.../docs/styles/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('.../docs/styles'));
+// // Concatenate CSS Files
+// gulp.task('css', function () {
+//   return gulp.src(['styles/*.css','node_modules/normalize.css/normalize.css'])
+//     .pipe(concatCss("all.css"))
+//     .pipe(gulp.dest('../docs/styles'));
+// });
+
+// // Minify CSS (packs for file size.)
+// gulp.task('minify-css', () => {
+//   return gulp.src('.../docs/styles/*.css')
+//     .pipe(cleanCSS({compatibility: 'ie8'}))
+//     .pipe(gulp.dest('.../docs/styles'));
+// });
+
+// Parse build blocks + uglify css and js.
+gulp.task('docs', function (){
+  return gulp.src('*.html')
+  .pipe(useref())
+  .pipe(gulp.dest('../docs'))
 });
 
 // Browsersync (best for testing during development.)
-gulp.task('browser-sync',['css'],function() {
+gulp.task('browser-sync',function() {
     browserSync.init(["styles/*.css", "js/*.js"], {
         server: {
             baseDir: "./"
@@ -56,8 +65,4 @@ gulp.task('browser-sync',['css'],function() {
 });
 
  // Default Task (runs all of this stuff.)
-gulp.task('default', ['scripts','sass','css','minify-css','browser-sync']);
-
-
-
- // TODO
+gulp.task('default', ['docs','browser-sync']);
