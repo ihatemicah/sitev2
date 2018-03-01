cf3a9483b98bd10bb0cb621c9294a372121370b5
Micah Carroll <micahcarroll@micahs-mbp.lan>
2018-02-28 21:51:53 -0800

    intro paragraph + navigation complete on desktop

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// On load fade in animations.
+window.sr = ScrollReveal();
+sr.reveal('.intro-paragraph');
+sr.reveal('.em-art',{delay: 100});
+sr.reveal('.em-thinking_face',{delay: 400});
+sr.reveal('.em-video_game',{delay: 600});
+sr.reveal('.em-mage',{delay: 1000});
