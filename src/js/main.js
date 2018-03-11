ed30bbf72f71e56bdcd4887c01fa5d2a44a8c935
Micah <micahgester@gmail.com>
2018-03-11 01:01:18 -0800

    image functionality added + project hover states

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,5 @@ sr.reveal('.em-art',{delay: 100});
 sr.reveal('.em-thinking_face',{delay: 400});
 sr.reveal('.em-video_game',{delay: 600});
 sr.reveal('.em-mage',{delay: 1000});
+
+sr.reveal('.project-container',{delay: 100});
