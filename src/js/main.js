680f8a26a7d6ec066851549fbce36683bd9d2c67
Micah <micahgester@gmail.com>
2018-04-25 00:36:17 -0700

    page animations and fixed links

--- src/js/main.js
+++ src/js/main.js
@@ -9,20 +9,20 @@ sr.reveal('.em-video_game',{delay: 600});
 sr.reveal('.em-mage',{delay: 1000});
 
 sr.reveal('#yogi-tea',{delay: 400});
-sr.reveal('#anime-poster',{delay:800});
-sr.reveal('#ring',{delay:900});
-sr.reveal('#team-hex',{delay:800});
-sr.reveal('.from-them',{delay:2000});
-sr.reveal('.from-me',{delay:4000});
+sr.reveal('#anime-poster',{delay:400});
+sr.reveal('#ring',{delay:500});
+sr.reveal('#team-hex',{delay:400});
+sr.reveal('.from-them',{delay:400});
+sr.reveal('.from-me',{delay:500});
 sr.reveal('#social-media',{delay:400});
 
-// Detailed page.
+  // Detailed page.
 sr.reveal('.team-hex-hero',{delay:800});
 sr.reveal('.detailed-intro-subparagraph',{delay:1000});
 sr.reveal('.product-detail-text-1',{delay:1500});
 sr.reveal('.product-detail-text-2',{delay:1500});
 
-// About me.
+  // About me.
 sr.reveal('.about-me',{delay:1000});
 
 // Tons of projects.
@@ -80,3 +80,27 @@ const looper = function() {
 
 
 $(document).ready(looper);
+
+// Page Transition Call Outs
+  $(".animsition-overlay").animsition({
+    inClass: 'overlay-slide-in-top',
+    outClass: 'overlay-slide-out-top',
+    inDuration: 1500,
+    outDuration: 800,
+    linkElement: '.animsition-link',
+    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+    loading: false,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: false,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    browser: [ 'animation-duration', '-webkit-animation-duration'],
+    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+    overlay : true,
+    overlayClass : 'animsition-overlay-slide',
+    overlayParentElement : 'body',
+    transition: function(url){ window.location.href = url; }
+  });
