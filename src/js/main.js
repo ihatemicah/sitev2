3fb494730a68bc3124ea77658bad4cd847293cb6
Micah Carroll <micahgester@gmail.com>
2022-02-02 22:40:55 -0500

    landing page complete, case studies updated and linked

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,14 @@ var a;
 function show_hide() {
   if(a==1)
     {
-      document.getElementById("reading-more").style.display="none";
-      document.getElementById("read-more-button").style.display="block";
+      document.getElementById("reading-more").style.display ="none";
+      document.getElementById("read-more-button").style.display ="block";
       return a=0;
     }
   else 
     {
-      document.getElementById("reading-more").style.display="flex";
-      document.getElementById("read-more-button").style.display="none";
+      document.getElementById("reading-more").style.display ="flex";
+      document.getElementById("read-more-button").style.display ="none";
       return a=1;
     }
 }
