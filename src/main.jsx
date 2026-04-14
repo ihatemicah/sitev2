fcc69de0eec555f6540ee4a96a27272068d7e31d
Micah Carroll <micahgester@gmail.com>
2026-04-14 15:44:28 -0700

    Fixing Favicon + Metadata

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,25 @@ import App from './App.jsx'
 
 gsap.registerPlugin(ScrollTrigger)
 
+function ensureHeadLink(rel, attrs) {
+  let el = document.querySelector(`link[rel="${rel}"]`)
+  if (!el) {
+    el = document.createElement('link')
+    el.rel = rel
+    document.head.appendChild(el)
+  }
+  Object.assign(el, attrs)
+}
+
+ensureHeadLink('icon', {
+  // Resolved in the browser from the bundle URL (not the document URL).
+  href: new URL(/* @vite-ignore */ '../favi-3.svg', import.meta.url).href,
+  type: 'image/svg+xml',
+})
+ensureHeadLink('apple-touch-icon', {
+  href: new URL(/* @vite-ignore */ '../favi.png', import.meta.url).href,
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
