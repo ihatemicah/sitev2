2cf9d51050956718ea6518602b1531b1084fab55
Micah Carroll <micahgester@gmail.com>
2026-04-14 15:13:10 -0700

    Trying Actions

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  // Project Pages URL: https://ihatemicah.github.io/sitev2/
-  base: command === 'build' ? '/sitev2/' : '/',
+export default defineConfig({
   plugins: [
     react({
       babel: {
@@ -12,4 +10,4 @@ export default defineConfig(({ command }) => ({
       },
     }),
   ],
-}))
+})
